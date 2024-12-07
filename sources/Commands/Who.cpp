af81226c3001add7ac7980727bdc4f657ebf04bd
Ali Imran Bozkurt <albozkur@k1m03s03.42kocaeli.com.tr>
2024-12-07 17:03:29 +0300

    a

---
+++ sources/Commands/Who.cpp
@@ -0,0 +1,31 @@
+#include "Channel.hpp"
+#include "Client.hpp"
+#include <string>
+#include <map>
+
+//:server 352 <nick> <channel> <user> <host> <server> <nick> <H|G>[*][@|+] :<hopcount> <realname>
+// <client> <channel> <username> <host> <server> <nick> <flags> :<hopcount> <realname>
+// :server 315 <nick> <mask> :End of WHO list
+#define RPL_WHOREPLY(client, channel, username, host, target, status, realname) ": 352 " + client + " " + channel + " " + username + " " + host + " " + target + " " + status + " :0 " + realname
+
+//#define RPL_WHOREPLY(client, channel, username, host, target, status, realname) ": 352 " + client + " " + channel + " " + username + " " + host + " " + target + " " + status + " :0 " + realname
+//#define RPL_WHOREPLY(client, channel,username, host,target, status, realname)   ": 352 " + client + " " + channel + " " + username + " " + host + " " + "localhost "  + target +  " H"+status + ":0 " + realname
+#define RPL_ENDOFWHO(client, channel)                                     ": 315 " + client + " " + channel + " :End of WHO list"
+
+void    who(std::map<std::string, Channel> &channels, Client client,  std::vector<std::string> &cmd)
+{
+    if (cmd.size() != 2)
+        client.MsgToClient(ERR_NEEDMOREPARAMS(client.getNickname(), cmd[0])); // kalan parametreler eklencek
+    else if (channels.find(cmd[1]) == channels.end())
+        client.MsgToClient(ERR_NOSUCHCHANNEL(client.getPrefixName(), cmd[1]));
+    else
+    {
+        std::vector<Client>::iterator user = channels[cmd[1]].getClients().begin();
+        for (; user != channels[cmd[1]].getClients().end(); ++user)
+        {
+            std::string status = channels[cmd[1]].IsOperator(*user) ? "O" : "H";
+            client.MsgToClient(RPL_WHOREPLY(client.getNickname(), cmd[1], client.getUsername() ,client.getHostName(), user->getNickname(),status, user->getRealName()));
+        }
+        client.MsgToClient(RPL_ENDOFWHO(client.getNickname(), cmd[1]));
+    }
+}
